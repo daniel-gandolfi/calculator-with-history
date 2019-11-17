22d9d36744b2eaac038afda84067a0ea9a77e67a
Daniel Gandolfi <gandan97@hotmail.it>
2019-11-17 13:00:08 +0100

    refactoring with hooks

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,227 +1,185 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import CalculatorDisplay from "../CalculatorDisplay/CalculatorDisplay";
 import CalculatorButtonGrid from "../CalculatorButtonGrid/CalculatorButtonGrid";
 import {ADD, DIV, MIN, MUL} from "../MathOperations"
 import ExpressionNode from "../expression-graph/ExpressionNode";
 import ExpressionTreeCostructor
-    from "../expression-graph/ExpressionTreeCostructor";
+	from "../expression-graph/ExpressionTreeCostructor";
 import styles from "./Calculator.module.scss"
 import History from "../History";
 
 import {
-    AddExpressionNode,
-    DivExpressionNode,
-    MinExpressionNode,
-    MulExpressionNode
+	AddExpressionNode,
+	DivExpressionNode,
+	MinExpressionNode,
+	MulExpressionNode
 } from "../expression-graph/MathOperationNodes";
 
-function createDefaultState() {
-    return {
-        display: DEFAULT_DISPLAY_VALUE,
-        treeConstructor: new ExpressionTreeCostructor(),
-        isEqualJustPressed: false,
-        currentHistoryIterator: null,
-        isShowingHistory: false,
-        isClean: true
-    }
-}
+const mathOperationToExpressionNodeMap = new Map();
+
+mathOperationToExpressionNodeMap.set(ADD, AddExpressionNode);
+mathOperationToExpressionNodeMap.set(MIN, MinExpressionNode);
+mathOperationToExpressionNodeMap.set(MUL, MulExpressionNode);
+mathOperationToExpressionNodeMap.set(DIV, DivExpressionNode);
 
 const HISTORY_SIZE = 30;
 const DEFAULT_DISPLAY_VALUE = "0";
-class Calculator extends Component {
-    constructor() {
-        super();
-        this.state = Object.assign(
-            createDefaultState(),
-            {
-                history: new History(HISTORY_SIZE),
-                currentHistoryIterator: null
-            }
-        );
-    }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return (this.state.display !== nextState.display);
-    }
+function Calculator() {
+	const [display, setDisplay] = useState(DEFAULT_DISPLAY_VALUE);
+	const [history, setHistory] = useState(new History(HISTORY_SIZE));
+	const [currentHistoryIterator, setCurrentHistoryIterator] = useState(null);
+	const [treeConstructor, setTreeConstructor] = useState(new ExpressionTreeCostructor());
+	const [isEqualJustPressed, setEqualJustPressed] = useState(false);
+	const [isShowingHistory, setShowingHistory] = useState(false);
+	const [isClean, setClean] = useState(true);
 
-    onNumberPressed = (num) => {
+	function createDefaultState() {
+		setDisplay(DEFAULT_DISPLAY_VALUE);
+		setTreeConstructor(new ExpressionTreeCostructor());
+		setEqualJustPressed(false);
+		setCurrentHistoryIterator(null);
+		setShowingHistory(false);
+		setClean(true);
+	}
 
-        var clearField = this.state.isEqualJustPressed || this.state.isClean;
-        var displayValue;
-        if (clearField) {
-            displayValue = num
-        } else {
-            displayValue = this.state.display + "" + num
-        }
+	const onNumberPressed = (num) => {
 
-        this.resetHistoryIterator();
+		const clearField = isEqualJustPressed || isClean;
+		let displayValue;
+		if (clearField) {
+			displayValue = num
+		} else {
+			displayValue = display + "" + num
+		}
 
-        this.setState({
-            display: displayValue,
-            isEqualJustPressed: false,
-            isShowingHistory: false,
-            isClean: false
-        })
-    }
-    resetHistoryIterator = () => {
-        if (this.state.currentHistoryIterator != null) {
-            this.setState({
-                currentHistoryIterator: null
-            });
-        }
-    }
-    onDotPressed = () => {
-        if (("" + this.state.display).indexOf(".") === -1) {
-            this.resetHistoryIterator();
-            this.setState({
-                display: this.state.display + ".",
-                isEqualJustPressed: false,
-                isShowingHistory: false
-            })
-        }
-    }
-    isMinusSignTheOnlyCharVisible = () => {
-        const isMinusSignVisible = ("" + this.state.display).indexOf("-") !== -1;
-        return isMinusSignVisible && this.state.display.length === 1;
-    }
-    onOperationPressed = (mathOperation) => {
-        const tree = this.state.treeConstructor;
-        const lastNodeDescriptor = tree.getLastNodeTypeDescriptor();
-        var operationNode;
-        if (this.isMinusSignTheOnlyCharVisible()) {
-            return;
-        }
+		resetHistoryIterator();
 
-        if (this.state.isClean) {
-            if (mathOperation === MIN) {
-                this.setState({
-                    display: "-",
-                    tree,
-                    isEqualJustPressed: false,
-                    isShowingHistory: false,
-                    isClean: false
-                })
-            }
-        } else {
-            switch (mathOperation) {
-                case ADD:
-                    operationNode = new AddExpressionNode([]);
-                    break;
-                case MIN:
-                    operationNode = new MinExpressionNode([]);
-                    break;
-                case MUL:
-                    operationNode = new MulExpressionNode([]);
-                    break;
-                case DIV:
-                    operationNode = new DivExpressionNode([]);
-                    break;
-                default:
-                    return;
-            }
+		setDisplay(displayValue);
+		setEqualJustPressed(false);
+		setShowingHistory(false);
+		setClean(false)
+	};
+	const resetHistoryIterator = () => {
+		setCurrentHistoryIterator(null)
+	};
+	const onDotPressed = () => {
+		if (("" + display).indexOf(".") === -1) {
+			resetHistoryIterator();
+			setDisplay(display + ".");
+			setEqualJustPressed(false);
+			setShowingHistory(false);
+		}
+	};
+	const isMinusSignTheOnlyCharVisible = () => {
+		const isMinusSignVisible = ("" + display).indexOf("-") !== -1;
+		return isMinusSignVisible && display.length === 1;
+	};
+	const onOperationPressed = (mathOperation) => {
+		const tree = treeConstructor;
+		tree.getLastNodeTypeDescriptor();
+		let operationNode;
+		if (isMinusSignTheOnlyCharVisible()) {
+			return;
+		}
 
-            var number = Number(this.state.display);
-            tree.addNode(new ExpressionNode(number));
-            tree.addNode(operationNode);
-            this.setState({
-                display: DEFAULT_DISPLAY_VALUE,
-                tree,
-                isEqualJustPressed: false,
-                isShowingHistory: false,
-                isClean: true
-            })
-        }
-    }
-    onEqualPressed = () => {
-        const canComputeValue = !this.state.isShowingHistory && !this.state.isEqualJustPressed && !this.isMinusSignTheOnlyCharVisible();
-        if (canComputeValue) {
-            var number = Number(this.state.display);
-            var tree = this.state.treeConstructor;
-            tree.addNode(new ExpressionNode(number));
+		if (isClean) {
+			if (mathOperation === MIN) {
+				setDisplay("-");
+				setTreeConstructor(tree);
+				setEqualJustPressed(false);
+				setShowingHistory(false);
+				setClean(false)
+			}
+		} else {
+			let expressionNodeConstructor = mathOperationToExpressionNodeMap.get(mathOperation);
+			operationNode = new expressionNodeConstructor([]);
 
-            var computedValue = tree.getRoot().resolve();
-            this.state.history.add(computedValue);
-            this.setState({
-                display: computedValue,
-                treeConstructor: new ExpressionTreeCostructor(),
-                isEqualJustPressed: true,
-                isClean: false
-            })
-        }
-    }
-    reset = () => {
-        this.resetHistoryIterator();
-        this.setState(createDefaultState())
-    }
-    deleteLastChar = () => {
-        const display = "" + this.state.display;
-        if (display !== DEFAULT_DISPLAY_VALUE) {
-            this.setState({
-                display: (display.length !== 1 ? display.substr(0, display.length - 1) : DEFAULT_DISPLAY_VALUE),
-                isClean: display.length === 1
-            })
-        }
-    };
-    handleHistoryExtraction = (savedValue) => {
-        if (savedValue) {
-            const newTree = new ExpressionNode(savedValue);
-            this.setState({
-                treeConstructor: new ExpressionTreeCostructor(),
-                display: newTree.resolve(),
-                isShowingHistory: true,
-                isEqualJustPressed: false,
-                isClean: false
-            })
-        }
-    };
-    getOrCreateHistoryIterator = () => {
-        var currentHistoryIterator = this.state.currentHistoryIterator;
-        if (!currentHistoryIterator) {
-            currentHistoryIterator = this.state.history.getIterator();
-            this.setState({
-                currentHistoryIterator
-            })
-        }
-        return currentHistoryIterator;
-    };
-    onBackPressed = () => {
-        const currentHistoryIterator = this.getOrCreateHistoryIterator();
-        if (currentHistoryIterator && currentHistoryIterator.hasPrevious()) {
-            if (this.state.isEqualJustPressed) {
-                currentHistoryIterator.last();
-            }
-            this.handleHistoryExtraction(currentHistoryIterator.previous());
-        }
-    };
-    onForwardPressed = () => {
-        const currentHistoryIterator = this.getOrCreateHistoryIterator();
-        if (currentHistoryIterator && currentHistoryIterator.hasNext()) {
-            this.handleHistoryExtraction(currentHistoryIterator.next());
-        }
-    };
+			const number = Number(display);
+			tree.addNode(new ExpressionNode(number));
+			tree.addNode(operationNode);
+			setDisplay(DEFAULT_DISPLAY_VALUE);
+			setTreeConstructor(tree);
+			setEqualJustPressed(false);
+			setShowingHistory(false);
+			setClean(true);
+		}
+	};
+	const onEqualPressed = () => {
+		const canComputeValue = !isShowingHistory && !isEqualJustPressed && !isMinusSignTheOnlyCharVisible();
+		if (canComputeValue) {
+			const number = Number(display);
+			treeConstructor.addNode(new ExpressionNode(number));
 
-    getBtnGridComponent = (() => {
-        var component = (
-            <CalculatorButtonGrid
-              onNumberPressed={this.onNumberPressed}
-              onOperationPressed={this.onOperationPressed}
-              onEqualPressed={this.onEqualPressed}
-              onDotPressed={this.onDotPressed}
-              onResetPressed={this.reset}
-              onDeletePressed={this.deleteLastChar}
-              onBackPressed={this.onBackPressed}
-              onForwardPressed={this.onForwardPressed}
-            />
-        );
-        return () => component;
-    })();
+			const computedValue = treeConstructor.getRoot().resolve();
+			history.add(computedValue);
+			setHistory(history);
+			setDisplay(computedValue);
+			setTreeConstructor(new ExpressionTreeCostructor());
+			setEqualJustPressed(true);
+			setClean(false)
+		}
+	};
+	const reset = () => {
+		resetHistoryIterator();
+		createDefaultState();
+	};
+	const deleteLastChar = () => {
+		const displayValue = "" + display;
+		if (displayValue !== DEFAULT_DISPLAY_VALUE) {
+			setDisplay(displayValue.length !== 1 ? displayValue.substr(0, displayValue.length - 1) : DEFAULT_DISPLAY_VALUE);
+			setClean(displayValue.length === 1)
+		}
+	};
+	const handleHistoryExtraction = (iterator, savedValue) => {
+		if (savedValue) {
+			const newTree = new ExpressionNode(savedValue);
+			setCurrentHistoryIterator(iterator);
+			setTreeConstructor(new ExpressionTreeCostructor());
+			setDisplay(newTree.resolve());
+			setShowingHistory(true);
+			setEqualJustPressed(false);
+			setClean(false);
+		}
+	};
+	const createHistoryIterator = () => {
+		return history.getIterator();
+	};
+	const onBackPressed = () => {
+		let iterator = currentHistoryIterator;
+		if (!iterator) {
+			iterator = createHistoryIterator();
+		}
+		if (iterator && iterator.hasPrevious()) {
+			if (isEqualJustPressed) {
+				iterator.last();
+			}
+			handleHistoryExtraction(iterator, iterator.previous());
+		}
+	};
+	const onForwardPressed = () => {
+		let iterator = currentHistoryIterator;
+		if (!iterator) {
+			iterator = createHistoryIterator();
+		}
+		if (iterator && iterator.hasNext()) {
+			handleHistoryExtraction(iterator, iterator.next());
+		}
+	};
 
-    render() {
-        return <div className={styles.container}>
-            <CalculatorDisplay value={this.state.display}/>
-            {this.getBtnGridComponent()}
-        </div>
-    }
+	return <div className={styles.container}>
+		<CalculatorDisplay value={display}/>
+		<CalculatorButtonGrid
+			onNumberPressed={onNumberPressed}
+			onOperationPressed={onOperationPressed}
+			onEqualPressed={onEqualPressed}
+			onDotPressed={onDotPressed}
+			onResetPressed={reset}
+			onDeletePressed={deleteLastChar}
+			onBackPressed={onBackPressed}
+			onForwardPressed={onForwardPressed}
+		/>
+	</div>
 }
 
 export default Calculator
--- src/CalculatorButtonGrid/CalculatorButtonGrid.js
+++ src/CalculatorButtonGrid/CalculatorButtonGrid.js
@@ -1,7 +1,21 @@
 import React from "react";
-import partial from "lodash/partial"
+import {useCallback} from "react";
 import {ADD, MIN,MUL, DIV} from "../MathOperations"
 import styles from "./CalculatorButtonGrid.module.scss"
+
+const numberList = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+];
+
 function CalculatorButtonGrid (props){
     const {
         onNumberPressed,
@@ -13,28 +27,38 @@ function CalculatorButtonGrid (props){
         onDeletePressed,
         onResetPressed
     } = props;
+
+    const addPressed = useCallback(() => onOperationPressed(ADD), [onOperationPressed]),
+      minPressed = useCallback(() => onOperationPressed(MIN), [onOperationPressed]),
+      mulPressed = useCallback(() => onOperationPressed(MUL), [onOperationPressed]),
+      divPressed = useCallback(() => onOperationPressed(DIV), [onOperationPressed]);
+
+    const numberCallbackList = numberList.map(function (number) {
+        return useCallback(()=>onNumberPressed(number), [onNumberPressed])
+    });
+
     return <div className={styles.container}>
         <button className={styles["button--operation"]} onClick={onBackPressed}>&#x27F2;</button>
         <button className={styles["button--operation"]} onClick={onForwardPressed}>&#x27F3;</button>
         <button className={styles["button--operation"]} onClick={onDeletePressed}>&#x232B;</button>
-        <button className={styles["button--math-operation"]} onClick={partial(onOperationPressed,DIV)}>/</button>
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,1)}>1</button>
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,2)}>2</button>
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,3)}>3</button>
-        <button className={styles["button--math-operation"]} onClick={partial(onOperationPressed,ADD)}>+</button>
+        <button className={styles["button--math-operation"]} onClick={divPressed}>/</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[1]}>1</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[2]}>2</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[3]}>3</button>
+        <button className={styles["button--math-operation"]} onClick={addPressed}>+</button>
 
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,4)}>4</button>
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,5)}>5</button>
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,6)}>6</button>
-        <button className={styles["button--math-operation"]} onClick={partial(onOperationPressed,MIN)}>-</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[4]}>4</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[5]}>5</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[6]}>6</button>
+        <button className={styles["button--math-operation"]} onClick={minPressed}>-</button>
 
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,7)}>7</button>
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,8)}>8</button>
-        <button className={styles["button--number"]} onClick={partial(onNumberPressed,9)}>9</button>
-        <button className={styles["button--math-operation"]} onClick={partial(onOperationPressed,MUL)}>x</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[7]}>7</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[8]}>8</button>
+        <button className={styles["button--number"]} onClick={numberCallbackList[9]}>9</button>
+        <button className={styles["button--math-operation"]} onClick={mulPressed}>x</button>
 
         <button className={styles["button--operation"]} onClick={onResetPressed}>AC</button>
-        <button className={styles["button--zero"]} onClick={partial(onNumberPressed,0)}>0</button>
+        <button className={styles["button--zero"]} onClick={numberCallbackList[0]}>0</button>
         <button className={styles["button--dot"]} onClick={onDotPressed}>.</button>
         <button className={styles["button--math-operation"]} onClick={onEqualPressed}>=</button>
     </div>
